c513a99c315d924c7ef934da352f3e7791a40a5b
lleellee0 <lleellee013@gmail.com>
2019-03-27 00:46:41 +0900

    환경변수로 입력하는 코드 추가, 환경변수 미입력시 디폴트 값으로 실행

--- conf/db-conf.js
+++ conf/db-conf.js
@@ -1,7 +1,7 @@
 exports.module = {
   connectionLimit: 100,
-  host: 'localhost',
-  user: 'root',
-  password: 'mysql123',
+  host: process.env.DB_HOST_IP ? process.env.DB_USER_NAME : 'localhost',
+  user: process.env.DB_USER_NAME ? process.env.DB_USER_NAME : 'root',
+  password: process.env.DB_USER_PASSWORD ? process.env.DB_USER_PASSWORD : 'mysql123',
   database: 'bittrex_acc_bot'
 };
