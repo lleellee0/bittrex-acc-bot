efcbfe37da78fcd54f41cc5b65912da47bfd1e50
lleellee0 <lleellee013@gmail.com>
2019-03-26 23:23:23 +0900

    sum_volume.js에서 오타 수정 // delete_over_week_history 추가로 1주일이 지난 로그 제거

--- bin.js
+++ bin.js
@@ -2,6 +2,7 @@ require('./conf/init.js');
 
 require('./getting-list/check-list.js');
 require('./getting-acc/get-history.js');
-require('./getting-acc/sum_volume.js'); // 이 부분에 문제가 있는 것으로 식별
+require('./getting-acc/sum_volume.js');
+require('./getting-acc/delete_over_week_history.js');
 
 require('./api/app.js');
